26308d6517d2cb6bd47384909426caad4c42baef
Ridhwankiki <80313535+Ridhwankiki@users.noreply.github.com>
2024-05-22 11:48:21 +0700

    Make Backend for Operation Network

---
+++ src/main/java/com/damas/repository/OperationNetworkRepository.java
@@ -0,0 +1,13 @@
+package com.damas.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.damas.model.OperationNetwork;
+
+
+
+public interface OperationNetworkRepository extends JpaRepository<OperationNetwork, String> {
+
+}
