c0064635c21c3939d16e31d909da0c5bae836b93
Ridhwankiki <80313535+Ridhwankiki@users.noreply.github.com>
2024-05-30 13:59:35 +0700

    merging

--- src/main/java/com/damas/repository/LogisticMemoRepository.java
+++ src/main/java/com/damas/repository/LogisticMemoRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.damas.model.LogisticMemo;
-import com.damas.model.ProjectDev;
-
 
 public interface LogisticMemoRepository extends JpaRepository<LogisticMemo, String> { // karena udh pake jpa repo jd pake interface
 @Query(value = "SELECT * FROM logistic_memo WHERE UPPER(memo_perihal) LIKE UPPER(CONCAT('%', ?1, '%')) OR UPPER(memo_pic) LIKE UPPER(CONCAT('%', ?1, '%'))", nativeQuery = true)
