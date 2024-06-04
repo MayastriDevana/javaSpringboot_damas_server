a8f089d78c2bc3393e145e4fa4528de816e4a7b8
Ridhwankiki <80313535+Ridhwankiki@users.noreply.github.com>
2024-06-04 09:22:36 +0700

    merging

--- src/main/java/com/damas/dbbcassdmdev/repository/UsersBcasRepository.java
+++ src/main/java/com/damas/dbbcassdmdev/repository/UsersBcasRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import com.damas.dbbcassdmdev.model.Users;
 
 public interface UsersBcasRepository extends JpaRepository<Users, String> {
-@Query(value = "SELECT nama, departemen FROM users", nativeQuery = true)
+@Query(value = "SELECT * FROM users", nativeQuery = true)
 List<Users> findNamaAndDeptInUsers();
     
 }
