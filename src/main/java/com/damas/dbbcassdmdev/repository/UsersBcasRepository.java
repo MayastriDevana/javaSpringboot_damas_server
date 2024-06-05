1f09bad143bbba2120907848cb39ebad1710b65b
Mayna <157772370+MayastriDevana@users.noreply.github.com>
2024-06-05 10:30:54 +0700

    change query projectphase

--- src/main/java/com/damas/dbbcassdmdev/repository/UsersBcasRepository.java
+++ src/main/java/com/damas/dbbcassdmdev/repository/UsersBcasRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import com.damas.dbbcassdmdev.model.Users;
 
 public interface UsersBcasRepository extends JpaRepository<Users, String> {
-@Query(value = "SELECT * FROM users", nativeQuery = true)
+@Query(value = "SELECT * FROM users WHERE satuan_kerja='Satuan Kerja Teknologi Informasi dan Logisitk'", nativeQuery = true)
 List<Users> findNamaAndDeptInUsers();
     
 }
