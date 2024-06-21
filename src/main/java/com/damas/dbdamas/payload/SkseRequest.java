52ec08c9eab91575cda385f2223d2f12d92bc70f
Mayna <157772370+MayastriDevana@users.noreply.github.com>
2024-06-21 16:04:44 +0700

    housekeeping sksereq

--- src/main/java/com/damas/dbdamas/payload/SkseRequest.java
+++ src/main/java/com/damas/dbdamas/payload/SkseRequest.java
@@ -17,26 +17,26 @@ public class SkseRequest {
     @Size(max = 100)
     private String id;
     
-    @NotBlank
+  
     @Size(max = 100)
     private String nosurat;
 
-    @NotBlank
+   
     @Size(max = 100)
     private String perihal;
 
-    @NotBlank
+   
     @Size(max = 100)
     private String pic;
 
     @Size(max = 100)
     private String departement;
 
-    @NotBlank
+   
     @Size(max = 100)
     private String deadline;
 
-    @NotBlank
+  
     @Size(max = 100)
     private String status;
 }
